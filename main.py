b7e16ce02cb4c7e094f6a21a39e85002a0ef0c06
Stranix <Phantom2525@gmail.com>
2023-08-03 18:12:02 +0300

    Добавил работу с корзиной.
    Добавление продуктов в корзину.
    Получение продуктов из корзины.

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 import logging
 
+import requests.exceptions
+
 from config.settings import settings
-from utils import generate_elastic_token, get_products_in_catalog
+
+from utils import generate_elastic_token, get_products_in_catalog, \
+    add_product_to_cart, get_cart_items
 
 logger = logging.getLogger('fish_bot')
 
 
 def main():
-    logger.info('Запуск приложения')
-    token, expire_in = generate_elastic_token()
-    products = get_products_in_catalog(token)
+    try:
+        logger.info('Запуск приложения')
+        token, expire_in = generate_elastic_token()
+        products = get_products_in_catalog(token)
+        product_id = products[0]['id']
+        cat_id = '123132dfsdf'
+        add_product_to_cart(token, cat_id, product_id, 1)
+        get_cart_items(token, cat_id)
+    except requests.exceptions.HTTPError as error:
+        logger.error('ошибка добавления %s', error.response.json())
 
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -57,3 +57,61 @@ def get_products_in_catalog(token):
     logger.debug('products: %s', products)
     logger.info('Продукты получены')
     return products
+
+
+def add_product_to_cart(token, cart_id, product_id, quantity):
+    logger.info('Добавление продуктов в корзину')
+    logger.debug(
+        'cart_id: %s, product_id: %s, quantity: %s',
+        cart_id,
+        product_id,
+        quantity
+    )
+    url = urllib.parse.urljoin(
+        settings.elastic_base_url,
+        f'/v2/carts/{cart_id}/items'
+    )
+    headers = {
+        'Authorization': f'Bearer {token}',
+        'Content-Type': 'application/json',
+        'EP-Channel': 'web store',
+        'EP-Context-Tag': 'clearance'
+    }
+    payload = {
+        'data': {
+            'id': product_id,
+            'type': 'cart_item',
+            'quantity': quantity,
+        }
+    }
+    logger.debug('url: %s', url)
+    logger.debug('headers: %s', headers)
+    logger.debug('payload: %s', payload)
+
+    response = requests.post(url, headers=headers, json=payload)
+    response.raise_for_status()
+    logger.info('Продукты добавлены.')
+    product_in_cart = response.json()['data']
+    logger.debug('product_in_cart: %s', product_in_cart)
+    return product_in_cart
+
+
+def get_cart_items(token, cart_id):
+    logger.info('Получение продуктов в корзине')
+    url = urllib.parse.urljoin(
+        settings.elastic_base_url,
+        f'/v2/carts/{cart_id}/items'
+    )
+    headers = {
+        'Authorization': f'Bearer {token}',
+    }
+    logger.debug('url: %s', url)
+    logger.debug('headers: %s', headers)
+
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+    cart_items = response.json()['data']
+    logger.info('Продукты в корзине получены')
+    logger.debug('cart_items: %s', response.json())
+
+    return cart_items
